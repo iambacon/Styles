cce3d07b3e832f7576f6cfd93462a0672b4f8d50
iambacon <github@iambacon.co.uk>
2014-07-05 18:40:01 +0100

    Started creating styleguide for website refresh

---
+++ src/IAmBacon/IAmBacon/Controllers/StyleguideController.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Mvc;
+
+namespace IAmBacon.Controllers
+{
+    public class StyleguideController : Controller
+    {
+        //
+        // GET: /Styleguide/
+
+        public ActionResult Index()
+        {
+            return View("Styleguide");
+        }
+
+    }
+}
