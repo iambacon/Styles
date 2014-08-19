e708f142bcaa209a46bcee3a335c44885d09d975
iambacon <github@iambacon.co.uk>
2014-08-19 22:15:09 +0100

    Added CssRewriteUrlTransform to bundles so that it transforms relative urls correctly

--- src/IAmBacon/IAmBacon/App_Start/BundleConfig.cs
+++ src/IAmBacon/IAmBacon/App_Start/BundleConfig.cs
@@ -22,35 +22,35 @@ namespace IAmBacon
         {
             bundles.Add(new StyleBundle("~/bundles/landingpageCss")
                 .Include("~/Content/css/modules/normalize.css")
-                .Include("~/Content/stylesheets/pages/post/landingpage.css"));
+                .Include("~/Content/stylesheets/pages/post/landingpage.css", new CssRewriteUrlTransform()));
 
             bundles.Add(new StyleBundle("~/bundles/landingpageIeCss")
                 .Include("~/Content/css/modules/normalize.css")
-                .Include("~/Content/stylesheets/pages/post/landingpage-ie.css"));
+                .Include("~/Content/stylesheets/pages/post/landingpage-ie.css", new CssRewriteUrlTransform()));
 
             bundles.Add(new StyleBundle("~/bundles/postCss")
                 .Include("~/Content/css/modules/normalize.css")
-                .Include("~/Content/stylesheets/pages/post/post.css"));
+                .Include("~/Content/stylesheets/pages/post/post.css", new CssRewriteUrlTransform()));
 
             bundles.Add(new StyleBundle("~/bundles/postIeCss")
                 .Include("~/Content/css/modules/normalize.css")
-                .Include("~/Content/stylesheets/pages/post/post-ie.css"));
+                .Include("~/Content/stylesheets/pages/post/post-ie.css", new CssRewriteUrlTransform()));
 
             bundles.Add(new StyleBundle("~/bundles/categoryCss")
                 .Include("~/Content/css/modules/normalize.css")
-                .Include("~/Content/stylesheets/pages/post/category.css"));
+                .Include("~/Content/stylesheets/pages/post/category.css", new CssRewriteUrlTransform()));
 
             bundles.Add(new StyleBundle("~/bundles/categoryIeCss")
                 .Include("~/Content/css/modules/normalize.css")
-                .Include("~/Content/stylesheets/pages/post/category-ie.css"));
+                .Include("~/Content/stylesheets/pages/post/category-ie.css", new CssRewriteUrlTransform()));
 
             bundles.Add(new StyleBundle("~/bundles/tagCss")
                 .Include("~/Content/css/modules/normalize.css")
-                .Include("~/Content/stylesheets/pages/post/tag.css"));
+                .Include("~/Content/stylesheets/pages/post/tag.css", new CssRewriteUrlTransform()));
 
             bundles.Add(new StyleBundle("~/bundles/tagIeCss")
                 .Include("~/Content/css/modules/normalize.css")
-                .Include("~/Content/stylesheets/pages/post/tag-ie.css"));
+                .Include("~/Content/stylesheets/pages/post/tag-ie.css", new CssRewriteUrlTransform()));
 
             bundles.Add(new StyleBundle("~/bundles/homeCss")
                 .Include("~/Content/stylesheets/pages/home/home.css", new CssRewriteUrlTransform()));
