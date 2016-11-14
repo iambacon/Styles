b64a32adedf34c2d1021d3e5149310201501538e
iambacon <iambacon@BACONNAISE>
2016-11-14 20:03:59 +0000

    Add attribute to force https across the site as a permanent redirect

--- src/IAmBacon/IAmBacon/App_Start/FilterConfig.cs
+++ src/IAmBacon/IAmBacon/App_Start/FilterConfig.cs
@@ -10,6 +10,7 @@
         public static void RegisterGlobalFilters(GlobalFilterCollection filters)
         {
             AddSearchEngineOptimizationFilters(filters);
+            AddSecurityFilters(filters);
         }
 
         private static void AddSearchEngineOptimizationFilters(GlobalFilterCollection filters)
@@ -18,6 +19,16 @@
                 new RedirectToCanonicalUrlAttribute(
                     RouteTable.Routes.AppendTrailingSlash,
                     RouteTable.Routes.LowercaseUrls));
+        }
+
+        private static void AddSecurityFilters(GlobalFilterCollection filters)
+        {
+            // Require HTTPS to be used across the whole site. System.Web.Mvc.RequireHttpsAttribute performs a
+            // 302 Temporary redirect from a HTTP URL to a HTTPS URL. This filter gives you the option to perform a
+            // 301 Permanent redirect or a 302 temporary redirect. You should perform a 301 permanent redirect if the
+            // page can only ever be accessed by HTTPS and a 302 temporary redirect if the page can be accessed over
+            // HTTP or HTTPS.
+            filters.Add(new RedirectToHttpsAttribute(true));
         }
     }
 }
