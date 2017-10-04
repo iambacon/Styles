b3e22995e469ad340b0e1f687a8a5aa02f9a731c
iambacon <iambacon@users.noreply.github.com>
2017-10-04 19:44:57 +0100

    Add Category entity test

---
+++ test/IAmBacon.Core.Domain.Tests/PostCategory/CategoryTests.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using IAmBacon.Core.Domain.PostCategory;
+using Machine.Specifications;
+
+namespace IAmBacon.Core.Domain.Tests.PostCategory
+{
+    [Subject("Category")]
+    public class When_argument_null
+    {
+        Because of = () => _exception = Catch.Exception(() => _sut = new Category(null));
+
+        It should_throw_exception = () => _exception.ShouldNotBeNull();
+
+        It should_be_of_type_ArgumentNullExeception = () => _exception.ShouldBeOfExactType<ArgumentNullException>();
+
+        static Exception _exception;
+        static Category _sut;
+    }
+}
