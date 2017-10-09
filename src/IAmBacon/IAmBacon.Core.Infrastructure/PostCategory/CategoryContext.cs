6457a69560055670bd431292cd2e1e7ba6091675
iambacon <iambacon@users.noreply.github.com>
2017-10-09 18:27:15 +0100

    Add infrastructure lib

---
+++ src/IAmBacon/IAmBacon.Core.Infrastructure/PostCategory/CategoryContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Threading.Tasks;
+using IAmBacon.Core.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace IAmBacon.Core.Infrastructure.PostCategory
+{
+    public class CategoryContext : DbContext, IUnitOfWork
+    {
+        public async Task CommitAsync()
+        {
+            int result = await base.SaveChangesAsync();
+        }
+    }
+}
