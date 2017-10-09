443323a0595cdae9e98d76d34bc6455a23ebfe69
iambacon <iambacon@users.noreply.github.com>
2017-10-09 18:27:15 +0100

    Add infrastructure for category entity

---
+++ src/IAmBacon/IAmBacon.Core.Infrastructure/PostCategory/CategoryContext.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using IAmBacon.Core.Domain.Interfaces;
+using IAmBacon.Core.Domain.PostCategory;
+using Microsoft.EntityFrameworkCore;
+
+namespace IAmBacon.Core.Infrastructure.PostCategory
+{
+    public class CategoryContext : DbContext, IUnitOfWork
+    {
+        public CategoryContext(DbContextOptions<CategoryContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Category> Categories { get; set; }
+
+        public async Task CommitAsync()
+        {
+            int result = await base.SaveChangesAsync();
+        }
+    }
+}
---
+++ src/IAmBacon/IAmBacon.Core.Infrastructure/PostCategory/Repositories/CategoryRepository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using IAmBacon.Core.Domain.Interfaces;
+using IAmBacon.Core.Domain.PostCategory;
+
+namespace IAmBacon.Core.Infrastructure.PostCategory.Repositories
+{
+    public class CategoryRepository : IRepository<Category>
+    {
+        private CategoryContext _context;
+
+        public CategoryRepository(CategoryContext context)
+        {
+            _context = context;
+        }
+
+        public IUnitOfWork UnitOfWork => _context;
+    }
+}
