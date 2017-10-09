89a2dd618aa451eed2be58dea2c32c1f55cb6dc2
iambacon <iambacon@users.noreply.github.com>
2017-10-09 20:32:06 +0100

    Add Admin project

---
+++ src/IAmBacon/IAmBacon.Admin/Controllers/CategoryController.cs
@@ -0,0 +1,42 @@
+﻿using System.Threading.Tasks;
+using IAmBacon.Admin.ViewModels;
+using IAmBacon.Core.Application.PostCategory.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IAmBacon.Admin.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly CategoryCommandHandler _handler;
+
+        public CategoryController(CategoryCommandHandler handler)
+        {
+            _handler = handler;
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(CreateCategoryViewModel model)
+        {
+            if (!ModelState.IsValid) return View(model);
+
+            try
+            {
+                var command = new CreateCategoryCommand(model.Name);
+
+                await _handler.HandleAsync(command);
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View(model);
+            }
+        }
+    }
+}
---
+++ src/IAmBacon/IAmBacon.Admin/ViewModels/CreateCategoryViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace IAmBacon.Admin.ViewModels
+{
+    public class CreateCategoryViewModel
+    {
+        public string Name { get; set; }
+    }
+}
