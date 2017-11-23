dc496a01a4dec9da1a01f966bff8fc778c72cf8e
iambacon <iambacon@users.noreply.github.com>
2017-11-23 20:16:05 +0000

    Add category autofac module for application layer

---
+++ src/IAmBacon/IAmBacon.Core.Application/AutofacModules/CategoryCommandModule.cs
@@ -0,0 +1,13 @@
+﻿using Autofac;
+using IAmBacon.Core.Application.PostCategory.Commands;
+
+namespace IAmBacon.Core.Application.AutofacModules
+{
+    public class CategoryCommandModule : Autofac.Module
+    {
+        protected override void Load(ContainerBuilder builder)
+        {
+            builder.RegisterType<CategoryCommandHandler>();
+        }
+    }
+}
