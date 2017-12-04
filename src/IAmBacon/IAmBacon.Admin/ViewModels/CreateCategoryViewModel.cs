d04831f414040899885b0f9fb31a740a4c7786ff
iambacon <iambacon@users.noreply.github.com>
2017-12-04 18:50:37 +0000

    Add validation rules to CreateCategoryViewModel

--- src/IAmBacon/IAmBacon.Admin/ViewModels/CreateCategoryViewModel.cs
+++ src/IAmBacon/IAmBacon.Admin/ViewModels/CreateCategoryViewModel.cs
@@ -1,7 +1,11 @@
-﻿namespace IAmBacon.Admin.ViewModels
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IAmBacon.Admin.ViewModels
 {
     public class CreateCategoryViewModel
     {
+        [Required]
+        [MaxLength(150)]
         public string Name { get; set; }
     }
 }
