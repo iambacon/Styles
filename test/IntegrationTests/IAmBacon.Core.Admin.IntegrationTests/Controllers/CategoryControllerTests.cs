8f53a531ffce9da14e84585f962a59e09086f871
iambacon <iambacon@users.noreply.github.com>
2018-01-26 08:55:37 +0000

    Add integration test for adding a category to the db

---
+++ test/IntegrationTests/IAmBacon.Core.Admin.IntegrationTests/Controllers/CategoryControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Linq;
+using IAmBacon.Admin.Controllers;
+using IAmBacon.Admin.ViewModels;
+using IAmBacon.Core.Application.PostCategory.Commands;
+using IAmBacon.Core.Infrastructure.PostCategory;
+using IAmBacon.Core.Infrastructure.PostCategory.Repositories;
+using Machine.Specifications;
+using Microsoft.EntityFrameworkCore;
+
+namespace IAmBacon.Core.Admin.IntegrationTests.Controllers
+{
+    [Subject("Category controller")]
+    public class When_adding_a_category
+    {
+        Establish context = () =>
+        {
+            _options = new DbContextOptionsBuilder<CategoryContext>()
+                .UseInMemoryDatabase(Guid.NewGuid().ToString())
+                .Options;
+        };
+
+        Because of = async () =>
+        {
+            using (var context = new CategoryContext(_options))
+            using (var controller = new CategoryController(new CategoryCommandHandler(new CategoryRepository(context))))
+            {
+                await controller.Create(new CreateCategoryViewModel { Name = "css" });
+            }
+        };
+
+        It should_write_to_the_db = () =>
+        {
+            using (var context = new CategoryContext(_options))
+            {
+                context.Categories.Count().ShouldEqual(1);
+            }
+        };
+
+        static DbContextOptions<CategoryContext> _options;
+    }
+}
