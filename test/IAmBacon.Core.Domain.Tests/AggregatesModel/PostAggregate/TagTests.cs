5eae33dd1048d5adde2c06758ce4ef97302536d5
Colin Bacon <iambacon@users.noreply.github.com>
2019-02-27 07:38:09 +0800

    Add tag entity tests

---
+++ test/IAmBacon.Core.Domain.Tests/AggregatesModel/PostAggregate/TagTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using IAmBacon.Core.Domain.AggregatesModel.PostAggregate;
+using Machine.Specifications;
+
+namespace IAmBacon.Core.Domain.Tests.AggregatesModel.PostAggregate
+{
+    [Subject("Tag")]
+    public class TagTests
+    {
+        public class When_argument_null
+        {
+            Because of = () => _exception = Catch.Exception(() => _sut = new Tag(null));
+
+            It should_throw_exception = () => _exception.ShouldNotBeNull();
+
+            It should_be_of_type_ArgumentNullExeception = () => _exception.ShouldBeOfExactType<ArgumentException>();
+
+            static Exception _exception;
+            static Tag _sut;
+        }
+    }
+}
