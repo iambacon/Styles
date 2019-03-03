303d0ef0b6a08b10ba6045d3e423b5074b443293
Colin Bacon <iambacon@users.noreply.github.com>
2019-03-04 06:56:16 +0800

    Add create HttpGet

---
+++ src/IAmBacon/IAmBacon.Admin/Controllers/TagController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using IAmBacon.Core.Application.PostTag.Commands;
+using Microsoft.AspNetCore.Mvc;
+
+namespace IAmBacon.Admin.Controllers
+{
+    public class TagController : Controller
+    {
+        private readonly TagCommandHandler _handler;
+
+        public TagController(TagCommandHandler handler)
+        {
+            _handler = handler ?? throw new ArgumentNullException(nameof(handler));
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View("Create");
+        }
+    }
+}
---
+++ src/IAmBacon/IAmBacon.Admin/ViewModels/Tag/CreateTagViewModel.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace IAmBacon.Admin.ViewModels.Tag
+{
+    public class CreateTagViewModel
+    {
+        [Required]
+        [MaxLength(150)]
+        public string Name { get; set; }
+    }
+}
