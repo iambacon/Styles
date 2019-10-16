79a639f0235d176591defc9d045ce35b674c46a8
Colin Bacon <iambacon@users.noreply.github.com>
2019-10-16 21:16:42 +0800

    Add indentity to infrastructure layer

---
+++ src/IAmBacon/IAmBacon.Core.Infrastructure/Identity/IdentityContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace IAmBacon.Core.Infrastructure.Identity
+{
+    public class IdentityContext : IdentityDbContext<IdentityUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options)
+            : base(options)
+        {
+        }
+    }
+}
