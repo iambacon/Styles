531dae014219ae6d49abc15b8b2cc6c29862a76d
Colin Bacon <iambacon@users.noreply.github.com>
2020-06-22 21:12:43 +0800

    build(grunt): add gruntfile
    Add gruntfile and dependencies to compile scss files to css. Includes post css tasks such as
    minify and autoprefixer.

---
+++ gruntfile.js
@@ -0,0 +1,72 @@
+'use strict';
+const sass = require('node-sass');
+
+module.exports = function (grunt) {
+    grunt.initConfig({
+
+        // Sass
+        sass: {
+            options: {
+                implementation: sass
+            },
+            dist: {
+                files: [
+                    {
+                        'dist/css/style.css': 'src/sass/style.scss'
+                    }
+                ]
+            }
+        },
+
+        // PostCSS
+        postcss: {
+            prod: {
+                options: {
+                    map: false,
+
+                    processors: [
+                        require('autoprefixer')(), // add vendor prefixes
+                        require('cssnano')() // minify the result
+                    ]
+                },
+                src: 'dist/css/style.css'
+            },
+            dev: {
+                options: {
+                    map: {
+                        inline: false
+                    },
+
+                    processors: [
+                        require('autoprefixer')() // add vendor prefixes
+                    ]
+                },
+                src: 'dist/css/style.css'
+            }
+        },
+
+        // Watch
+        watch: {
+            scss: {
+                files: 'src/sass/**/*.scss',
+                tasks: ['sass', 'postcss:dev'],
+                options: {
+                    spawn: false
+                }
+            }
+        }
+    });
+
+    // Load plugins
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-postcss');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+
+    grunt.registerTask('default',
+        'Compile all SCSS files, then watch for file changes and re-compile',
+        ['watch']);
+
+    grunt.registerTask('build',
+        'Compile all SCSS files minified',
+        ['sass', 'postcss:prod']);
+};
