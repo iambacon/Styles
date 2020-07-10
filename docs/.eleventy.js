9bd7a5d9f13211a517ec9d51a9c12b313d0ca7a3
Colin Bacon <iambacon@users.noreply.github.com>
2020-07-10 20:06:12 +0800

    docs(page): add Author
    Add content to the author page

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -2,5 +2,6 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = config => {
     config.addPassthroughCopy('assets/css');
+    config.addPassthroughCopy('images');
     config.addPlugin(syntaxHighlight);
 };
