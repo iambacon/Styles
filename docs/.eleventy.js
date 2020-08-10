cc77c241919ccb3b9d275933a34cc4592e7340aa
Colin Bacon <iambacon@users.noreply.github.com>
2020-08-10 13:13:37 +0800

    feat(fav): add fav icon
    Add fav icon to the site

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -2,6 +2,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
 module.exports = config => {
     config.addPassthroughCopy('assets/css');
+    config.addPassthroughCopy('assets/fav');
     config.addPassthroughCopy('images');
     config.addPlugin(syntaxHighlight);
 };
