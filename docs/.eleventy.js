76eeb4c8bd534f2710745c6668f6f3b54eed18e7
Colin Bacon <iambacon@users.noreply.github.com>
2020-08-16 11:47:15 +0800

    refactor(ui): add logo
    Add logo to the page header

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -3,6 +3,7 @@ const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 module.exports = config => {
     config.addPassthroughCopy('assets/css');
     config.addPassthroughCopy('assets/fav');
+    config.addPassthroughCopy('assets/images');
     config.addPassthroughCopy('images');
     config.addPassthroughCopy('web.config');
     config.addPlugin(syntaxHighlight);
