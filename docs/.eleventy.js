1d3f72ec5392029d72ce5356351c1704437e3c45
Colin Bacon <iambacon@users.noreply.github.com>
2021-11-24 15:17:12 +0800

    style(prettier): apply prettier formatting
    Apply prettier formatting to the project

--- docs/.eleventy.js
+++ docs/.eleventy.js
@@ -1,11 +1,11 @@
 const syntaxHighlight = require("@11ty/eleventy-plugin-syntaxhighlight");
 
-module.exports = config => {
-    config.addPassthroughCopy('assets/css');
-    config.addPassthroughCopy('assets/fav');
-    config.addPassthroughCopy('assets/images');
-    config.addPassthroughCopy('images');
-    config.addPassthroughCopy('web.config');
-    config.addPassthroughCopy('robots.txt');
-    config.addPlugin(syntaxHighlight);
+module.exports = (config) => {
+  config.addPassthroughCopy("assets/css");
+  config.addPassthroughCopy("assets/fav");
+  config.addPassthroughCopy("assets/images");
+  config.addPassthroughCopy("images");
+  config.addPassthroughCopy("web.config");
+  config.addPassthroughCopy("robots.txt");
+  config.addPlugin(syntaxHighlight);
 };
--- gruntfile.js
+++ gruntfile.js
@@ -1,165 +1,178 @@
-'use strict';
-const sass = require('node-sass');
+"use strict";
+const sass = require("node-sass");
 
 module.exports = function (grunt) {
-    grunt.initConfig({
-        // Shell commands
-        shell: {
-            eleventyBuild: {
-                command: 'npx @11ty/eleventy',
-                options: {
-                    stderr: false,
-                    execOptions: {
-                        cwd: 'docs',
-                        maxBuffer: 1000 * 1024,
-                    }
-                }
-            },
-            eleventyServe: {
-                command: 'npx @11ty/eleventy --serve',
-                options: {
-                    stderr: false,
-                    execOptions: {
-                        cwd: 'docs',
-                        maxBuffer: 1000 * 1024,
-                    }
-                }
-            },
-            zipSite: { 
-                command: 'powershell -command "Compress-Archive -Path docs/_site/* -DestinationPath dist/site.zip -Force"' 
-            }
+  grunt.initConfig({
+    // Shell commands
+    shell: {
+      eleventyBuild: {
+        command: "npx @11ty/eleventy",
+        options: {
+          stderr: false,
+          execOptions: {
+            cwd: "docs",
+            maxBuffer: 1000 * 1024,
+          },
         },
-
-        concurrent: {
-            options: {
-                logConcurrentOutput: true
-            },
-            dev: ['watch', 'shell:eleventyServe'],
-            sassCompile: ['sass', 'sass:styleguide']
+      },
+      eleventyServe: {
+        command: "npx @11ty/eleventy --serve",
+        options: {
+          stderr: false,
+          execOptions: {
+            cwd: "docs",
+            maxBuffer: 1000 * 1024,
+          },
         },
+      },
+      zipSite: {
+        command:
+          'powershell -command "Compress-Archive -Path docs/_site/* -DestinationPath dist/site.zip -Force"',
+      },
+    },
 
-        // Sass
-        sass: {
-            options: {
-                implementation: sass
-            },
-            dist: {
-                files: [
-                    {
-                        'docs/assets/css/style.css': 'src/sass/style.scss'
-                    }
-                ]
-            },
-            styleguide: {
-                files: [
-                    {
-                        'docs/assets/css/styleguide.css': 'docs/assets/sass/styleguide.scss'
-                    }
-                ]
-            },
-            prod: {
-                files: [
-                    {
-                        'dist/css/style.css': 'src/sass/style.scss'
-                    }
-                ]
-            }
+    concurrent: {
+      options: {
+        logConcurrentOutput: true,
+      },
+      dev: ["watch", "shell:eleventyServe"],
+      sassCompile: ["sass", "sass:styleguide"],
+    },
+
+    // Sass
+    sass: {
+      options: {
+        implementation: sass,
+      },
+      dist: {
+        files: [
+          {
+            "docs/assets/css/style.css": "src/sass/style.scss",
+          },
+        ],
+      },
+      styleguide: {
+        files: [
+          {
+            "docs/assets/css/styleguide.css":
+              "docs/assets/sass/styleguide.scss",
+          },
+        ],
+      },
+      prod: {
+        files: [
+          {
+            "dist/css/style.css": "src/sass/style.scss",
+          },
+        ],
+      },
+    },
+
+    // PostCSS
+    postcss: {
+      prod: {
+        options: {
+          map: false,
+
+          processors: [
+            require("autoprefixer")(), // add vendor prefixes
+            require("cssnano")(), // minify the result
+          ],
         },
+        src: "dist/css/style.css",
+      },
+      dev: {
+        options: {
+          map: {
+            inline: false,
+          },
 
-        // PostCSS
-        postcss: {
-            prod: {
-                options: {
-                    map: false,
-
-                    processors: [
-                        require('autoprefixer')(), // add vendor prefixes
-                        require('cssnano')() // minify the result
-                    ]
-                },
-                src: 'dist/css/style.css'
-            },
-            dev: {
-                options: {
-                    map: {
-                        inline: false
-                    },
-
-                    processors: [
-                        require('autoprefixer')() // add vendor prefixes
-                    ]
-                },
-                src: 'docs/assets/css/*.css'
-            },
-            docs: {
-                options: {
-                    map: false,
-
-                    processors: [
-                        require('autoprefixer')(),// add vendor prefixes
-                        require('cssnano')()// minify the result
-                    ]
-                },
-                src: 'docs/assets/css/*.css'
-            }
+          processors: [
+            require("autoprefixer")(), // add vendor prefixes
+          ],
         },
+        src: "docs/assets/css/*.css",
+      },
+      docs: {
+        options: {
+          map: false,
 
-        // Watch
-        watch: {
-            scss: {
-                files: 'src/sass/**/*.scss',
-                tasks: ['sass', 'postcss:dev'],
-                options: {
-                    spawn: false
-                }
-            },
-            styleguide: {
-                files: 'docs/assets/**/*.scss',
-                tasks: ['sass:styleguide'],
-                options: {
-                    spawn: false
-                }
-            }
+          processors: [
+            require("autoprefixer")(), // add vendor prefixes
+            require("cssnano")(), // minify the result
+          ],
         },
+        src: "docs/assets/css/*.css",
+      },
+    },
 
-        // Bump
-        bump: {
-            options: {
-              files: ['package.json'],
-              updateConfigs: [],
-              commit: true,
-              commitMessage: 'Release v%VERSION%',
-              commitFiles: ['package.json'],
-              createTag: true,
-              tagName: 'v%VERSION%',
-              tagMessage: 'Version %VERSION%',
-              push: false,
-              gitDescribeOptions: '--tags --always --abbrev=1 --dirty=-d',
-              globalReplace: false,
-              prereleaseName: false,
-              metadata: '',
-              regExp: false
-            }
-          }
-    });
+    // Watch
+    watch: {
+      scss: {
+        files: "src/sass/**/*.scss",
+        tasks: ["sass", "postcss:dev"],
+        options: {
+          spawn: false,
+        },
+      },
+      styleguide: {
+        files: "docs/assets/**/*.scss",
+        tasks: ["sass:styleguide"],
+        options: {
+          spawn: false,
+        },
+      },
+    },
 
-    // Load plugins
-    grunt.loadNpmTasks('grunt-shell');
-    grunt.loadNpmTasks('grunt-concurrent');
-    grunt.loadNpmTasks('grunt-sass');
-    grunt.loadNpmTasks('@lodder/grunt-postcss');
-    grunt.loadNpmTasks('grunt-contrib-watch');
-    grunt.loadNpmTasks('grunt-bump');
+    // Bump
+    bump: {
+      options: {
+        files: ["package.json"],
+        updateConfigs: [],
+        commit: true,
+        commitMessage: "Release v%VERSION%",
+        commitFiles: ["package.json"],
+        createTag: true,
+        tagName: "v%VERSION%",
+        tagMessage: "Version %VERSION%",
+        push: false,
+        gitDescribeOptions: "--tags --always --abbrev=1 --dirty=-d",
+        globalReplace: false,
+        prereleaseName: false,
+        metadata: "",
+        regExp: false,
+      },
+    },
+  });
 
-    grunt.registerTask('default',
-        'Compile all SCSS files and rebuild docs, then watch for file changes and re-compile',
-        ['concurrent:dev']);
+  // Load plugins
+  grunt.loadNpmTasks("grunt-shell");
+  grunt.loadNpmTasks("grunt-concurrent");
+  grunt.loadNpmTasks("grunt-sass");
+  grunt.loadNpmTasks("@lodder/grunt-postcss");
+  grunt.loadNpmTasks("grunt-contrib-watch");
+  grunt.loadNpmTasks("grunt-bump");
 
-    grunt.registerTask('build',
-        'Compile all SCSS files minified and output to dist folder',
-        ['sass:prod', 'postcss:prod']);
+  grunt.registerTask(
+    "default",
+    "Compile all SCSS files and rebuild docs, then watch for file changes and re-compile",
+    ["concurrent:dev"]
+  );
 
-    grunt.registerTask('deploy',
-        'Build documentation site and Zip ready for deployment',
-        ['concurrent:sassCompile', 'postcss:docs', 'shell:eleventyBuild', 'shell:zipSite']);
+  grunt.registerTask(
+    "build",
+    "Compile all SCSS files minified and output to dist folder",
+    ["sass:prod", "postcss:prod"]
+  );
+
+  grunt.registerTask(
+    "deploy",
+    "Build documentation site and Zip ready for deployment",
+    [
+      "concurrent:sassCompile",
+      "postcss:docs",
+      "shell:eleventyBuild",
+      "shell:zipSite",
+    ]
+  );
 };
